6e34b8fa3ad4f131cdc348d183d0693c656fdd99
Andrie de Vries <apdevries@gmail.com>
2016-03-18 15:06:11 +0000

    Use memoise() to cache results for speed improvement; Include plot_graph() as a place holder

--- R/compute_pagerank.R
+++ R/compute_pagerank.R
@@ -1,32 +1,58 @@
 #' @importFrom miniCRAN makeDepGraph
 #' @importFrom igraph page.rank
+#' @importFrom memoise memoise
+#' @importFrom utils available.packages
 NULL
 
+
+makeDepGraph <- function(pdb, pkg = pdb[, "Package"]){
+  miniCRAN::makeDepGraph(pkg,
+               availPkgs = pdb,
+               suggests=FALSE,
+               enhances=TRUE,
+               includeBasePkgs = FALSE)
+}
+
+getPackages <- function(mirror){
+  available.packages(
+    contrib.url(mirror, type = "source"),
+    type="source",
+    filters = NULL)  
+}
+
+getGraph <- memoise(makeDepGraph)
 
 #' Computes pagerank for CRAN packages.
 #'
 #' @param mirror CRAN mirror, e.g. http://cran.revolutionanalytics.com or https://mran.revolutionanalytics.com/snapshot/2016-03-15
+#' 
+#' @param decreasing If TRUE, sorts the result in decreasing order
 #'
 #' @example /inst/examples/examples_compute_pagerank.R
 #' @export
 compute_pagerank <- function(mirror, decreasing = TRUE){
-
-  pdb <- available.packages(
-    contrib.url(mirror, type = "source"),
-    type="source",
-    filters = NULL)
-
-    # Note that this step takes a while, expect ~15-30 seconds
-
-  g <-  makeDepGraph(pdb[, "Package"],
-                     availPkgs = pdb,
-                     suggests=FALSE,
-                     enhances=TRUE,
-                     includeBasePkgs = FALSE)
-
-  # Use the page.rank algorithm in igraph -----------------------------------
-
+  pdb <- getPackages(mirror)
+  g <- getGraph(pdb)
   pr <- page.rank(g, directed = FALSE)$vector
   if(decreasing) sort(pr, decreasing = TRUE) else pr
 }
 
+#' Plots the dependency graph
+#' 
+#' @inheritParams compute_pagerank
+#' @param top integer that indicates how many packages to include in the graph
+#' @param cex character expansion ratio for plot
+#' @param random.seed A random seed value. Used by igraph to initialise the plot layout. The function resets the seed to the original value, i.e. it will not have any impact on the random value stream in your R session.
+#' 
+#' @export
+plot_graph <- function(mirror, top = 25, cex = 0.5, random.seed = 1){
+  pdb <- getPackages(mirror)
+  g <- getGraph(pdb)
+  pr <- compute_pagerank(mirror, decreasing = TRUE)
+  pr <- names(head(pr, top))
+  pdb <- getGraph(pdb, pr)
+  old.seed <- .Random.seed
+  on.exit(.Random.seed <- old.seed)
+  set.seed(random.seed)
+  plot(pdb, main = sprintf("Top %s packages by page rank", length(pr)), cex = cex)
+}
--- inst/examples/examples_compute_pagerank.R
+++ inst/examples/examples_compute_pagerank.R
@@ -1,3 +1,8 @@
 MRAN <- "https://mran.revolutionanalytics.com/snapshot/2016-03-15/"
 pr <- compute_pagerank(MRAN)
-head(pr)
+head(pr, 10)
+
+plot_graph(MRAN, 10)
+plot_graph(MRAN, 10, cex = 0.8)
+plot_graph(MRAN, 25)
+plot_graph(MRAN, 50)
